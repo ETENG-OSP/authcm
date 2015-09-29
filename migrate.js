8e76bd29fdd16ce950235c7a005375c9d8d2f3d0
Gen Liu <gg@linux-o4mm.site>
2015-09-30 05:00:36 +0800

    add migration

---
+++ migrate.js
@@ -0,0 +1,24 @@
+var nconf = require('nconf');
+nconf.file({file: './config.development.json'});
+
+var models = require('./models');
+
+models
+  .sequelize
+  .sync()
+  .then(function() {
+    var Application = models.Application;
+    var data = require('./applications.json');
+    return Application.bulkCreate(data);
+  })
+  .then(function() {
+    var User = models.User;
+    var data = require('./users');
+    return User.bulkCreate(data.map(function(record) {
+      record.applicationId = '2';
+      return record;
+    }));
+  })
+  .then(function() {
+    console.log('done');
+  });
